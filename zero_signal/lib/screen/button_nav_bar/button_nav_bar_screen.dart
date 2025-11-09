a9dc3218954c4f3462dad8229f65d7e070eebe52
Rahat <nahidrahat007@gmail.com>
2025-11-10 04:37:07 +0600

    button nav bar completed

--- zero_signal/lib/screen/button_nav_bar/button_nav_bar_screen.dart
+++ zero_signal/lib/screen/button_nav_bar/button_nav_bar_screen.dart
@@ -1,12 +1,9 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:zero_signal/constant/app_colors.dart';
 import 'package:zero_signal/constant/app_icon_path.dart';
-import 'package:zero_signal/widget/text_widget/text_widgets.dart';
-import '../../widget/icon_widget/icon_widget.dart';
+import '../../widget/text_widget/text_widgets.dart';
 import 'controller/bottom_nav_controller.dart';
 
 class BottomNav extends StatelessWidget {
@@ -14,62 +11,6 @@ class BottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Screen dimensions
-    final screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    final screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
-    final isSmallDevice = screenWidth < 360;
-    final isMediumDevice = screenWidth >= 360 && screenWidth < 600;
-    final isLargeDevice = screenWidth >= 600;
-
-    // Responsive calculations
-    late double iconSelectedSize;
-    late double iconUnselectedSize;
-    late double navPadding;
-    late double navGap;
-    late double navIconSize;
-    late double fontSize;
-    late double borderRadius;
-
-    if (isSmallDevice) {
-      // Small devices (< 360px)
-      iconSelectedSize = screenWidth * 0.05;
-      iconUnselectedSize = screenWidth * 0.07;
-      navPadding = screenWidth * 0.03;
-      navGap = screenWidth * 0.01;
-      navIconSize = screenWidth * 0.065;
-      fontSize = screenWidth * 0.03;
-      borderRadius = 35;
-    } else if (isMediumDevice) {
-      // Medium devices (360-600px)
-      iconSelectedSize = screenWidth * 0.055;
-      iconUnselectedSize = screenWidth * 0.075;
-      navPadding = screenWidth * 0.040;
-      navGap = screenWidth * 0.015;
-      navIconSize = screenWidth * 0.07;
-      fontSize = screenWidth * 0.032;
-      borderRadius = 38;
-    } else {
-      // Large devices (> 600px)
-      iconSelectedSize = screenWidth * 0.12;
-      iconUnselectedSize = screenWidth * 0.09;
-      navPadding = screenWidth * 0.04;
-      navGap = screenWidth * 0.02;
-      navIconSize = screenWidth * 0.08;
-      fontSize = screenWidth * 0.035;
-      borderRadius = 40;
-    }
-
-    // Responsive padding
-    final horizontalPadding = isSmallDevice ? 12.0 : isMediumDevice
-        ? 14.0
-        : 16.0;
-
     return GetBuilder(
       init: UserBottomNavController(),
       builder: (controller) {
@@ -86,63 +27,49 @@ class BottomNav extends StatelessWidget {
                       .creamBackgroundColor : Colors.transparent
               ),
               padding: EdgeInsets.symmetric(
-                horizontal: horizontalPadding,
-                vertical: isSmallDevice ? 12 : 10,
+                horizontal: 14.w,
+                vertical: 10.h,
               ),
               child: Container(
-                width: Get.width,
+              
+             // height: 60.h,
 
-                color: Colors.red,
-
+                decoration: BoxDecoration(
+                  color: AppColor.buttonNavBackgroundColor,
+                  borderRadius: BorderRadius.circular(40.r),
+                ),
+                padding: EdgeInsets.symmetric(horizontal:8.w, vertical: 8.h),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-
-                    InkWell(
-
-                        onTap: (){
-
-                          controller.changeIndex(0);
-                        },
-                        child: TextWidget(
-                            fontColor: controller.selectedIndex.value == 0 ? AppColor.green : AppColor.textColor,
-                            text: "Home")),
-
-                    InkWell(
-
-                        onTap: (){
-
-                          controller.changeIndex(1);
-                        },
-                        child: TextWidget(
-                            fontColor: controller.selectedIndex.value == 1 ? AppColor.green : AppColor.textColor,
-
-                            text: "Home1")),
-
-                    InkWell(
-
-                        onTap: (){
-
-                          controller.changeIndex(2);
-                        },
-                        child: TextWidget(
-
-                            fontColor: controller.selectedIndex.value == 2 ? AppColor.green : AppColor.textColor,
-
-                            text: "Home2")),
-
-                    InkWell(
-
-                        onTap: (){
-
-                          controller.changeIndex(3);
-                        },
-                        child: TextWidget(
-
-                            fontColor: controller.selectedIndex.value == 3 ? AppColor.green : AppColor.textColor,
-
-                            text: "Home3")),
-
+                    _buildNavItem(
+                      controller: controller,
+                      index: 0,
+                      icon: AppIconPath.homeIcon,
+                      selectedIcon: AppIconPath.homeIconselect,
+                      label: "Explore",
+                    ),
+                    _buildNavItem(
+                      controller: controller,
+                      index: 1,
+                      icon: AppIconPath.routeIcon,
+                      selectedIcon: AppIconPath.routesSelect,
+                      label: "Routes",
+                    ),
+                    _buildNavItem(
+                      controller: controller,
+                      index: 2,
+                      icon: AppIconPath.socialIcon,
+                      selectedIcon: AppIconPath.socialSelect,
+                      label: "Social",
+                    ),
+                    _buildNavItem(
+                      controller: controller,
+                      index: 3,
+                      icon: AppIconPath.profileIcon,
+                      selectedIcon: AppIconPath.profileSelect,
+                      label: "Profile",
+                    ),
                   ],
                 ),
               ),
@@ -153,5 +80,61 @@ class BottomNav extends StatelessWidget {
     );
   }
 
-
+  Widget _buildNavItem({
+    required UserBottomNavController controller,
+    required int index,
+    required String icon,
+    required String selectedIcon,
+    required String label,
+  }) {
+    final isSelected = controller.selectedIndex.value == index;
+    
+    return GestureDetector(
+      onTap: () => controller.changeIndex(index),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        padding: EdgeInsets.symmetric(
+          horizontal: isSelected ? 12.w : 12.w,
+          vertical: 8.h,
+        ),
+        decoration: BoxDecoration(
+          color: isSelected 
+              ? AppColor.backgroundColor.withOpacity(0.9)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(30.r),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              isSelected ? selectedIcon : icon,
+              width: isSelected ? 24.r : 36.r,
+              height: isSelected ? 24.r : 36.r,
+            ),
+            AnimatedSize(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              child: SizedBox(
+                width: isSelected ? null : 0,
+                child: isSelected
+                    ? Padding(
+                        padding: EdgeInsets.only(left: 4.w),
+                        child: TextWidget(
+                         text: label,
+                         
+                          fontColor: AppColor.white500,
+                        
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    : null,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
